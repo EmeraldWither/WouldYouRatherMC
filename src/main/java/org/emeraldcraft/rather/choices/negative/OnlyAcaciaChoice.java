b67dc12ef3c425276c1097dd72304cf6fe2e06da
Ishaan <68785503+EmeraldWither@users.noreply.github.com>
2024-08-01 18:16:26 -0400

    add choice for only placing acacia planks

---
+++ src/main/java/org/emeraldcraft/rather/choices/negative/OnlyAcaciaChoice.java
@@ -0,0 +1,25 @@
+package org.emeraldcraft.rather.choices.negative;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.emeraldcraft.rather.choiceapi.Choice;
+
+public class OnlyAcaciaChoice extends Choice.ChoiceRunnable implements Listener {
+
+    public OnlyAcaciaChoice() {
+        super("You can only place acacia planks.");
+    }
+
+    @Override
+    public void run() {
+        registerEvents();
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!event.getPlayer().equals(getPlayer())) return;
+        if(event.getBlock().getType() != Material.ACACIA_PLANKS) event.setCancelled(true);
+    }
+}
